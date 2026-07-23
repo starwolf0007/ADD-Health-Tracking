abaf1b5e07b2bfad495570c2df20828b27cb3b93
starwolf0007 <94322221+starwolf0007@users.noreply.github.com>
2026-07-23 11:50:23 -0700

    Expose Health Connect permission methods to Dart

--- lib/platform/health_connect/health_connect_bridge.dart
+++ lib/platform/health_connect/health_connect_bridge.dart
@@ -4,6 +4,10 @@ import 'health_connect_models.dart';
 
 abstract interface class HealthConnectPlatform {
   Future<HealthConnectAvailability> getAvailability();
+
+  Future<Set<HealthConnectReadPermission>> getGrantedPermissions();
+
+  Future<Set<HealthConnectReadPermission>> requestPermissions();
 }
 
 class MethodChannelHealthConnectPlatform implements HealthConnectPlatform {
@@ -24,6 +28,29 @@ class MethodChannelHealthConnectPlatform implements HealthConnectPlatform {
       return HealthConnectAvailability.unsupported;
     } on MissingPluginException {
       return HealthConnectAvailability.unsupported;
+    }
+  }
+
+  @override
+  Future<Set<HealthConnectReadPermission>> getGrantedPermissions() async {
+    return _invokePermissionMethod('getGrantedPermissions');
+  }
+
+  @override
+  Future<Set<HealthConnectReadPermission>> requestPermissions() async {
+    return _invokePermissionMethod('requestPermissions');
+  }
+
+  Future<Set<HealthConnectReadPermission>> _invokePermissionMethod(
+    String method,
+  ) async {
+    try {
+      final value = await _channel.invokeMethod<Object?>(method);
+      return HealthConnectReadPermission.setFromWireValue(value);
+    } on PlatformException {
+      return const {};
+    } on MissingPluginException {
+      return const {};
     }
   }
 }
