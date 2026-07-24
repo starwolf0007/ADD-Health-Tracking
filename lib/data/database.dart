85a2341c93b91ed23df19c8a4c5f7822051b794b
Bryan <starwolf0007@gmail.com>
2026-07-24 13:41:42 -0700

    chore: restore analyzer-green baseline

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -10,7 +10,7 @@ import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
-import 'health_tables.dart';
+import 'package:neuroflow/data/health_tables.dart';
 
 part 'database.g.dart';
 
